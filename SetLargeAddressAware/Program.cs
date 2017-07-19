ad33613e44b3f2147cd9cab83bce9322b0456a21
Kirill Osenkov <github@osenkov.com>
2017-07-18 23:02:16 -0700

    Add SetLargeAddressAware console app.

---
+++ SetLargeAddressAware/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using PEFile;
+
+namespace SetLargeAddressAware
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                return 1;
+            }
+
+            var filePath = args[0];
+            if (!File.Exists(filePath))
+            {
+                return 2;
+            }
+
+            LargeAddressAware.SetLargeAddressAware(filePath);
+            return 0;
+        }
+    }
+}
